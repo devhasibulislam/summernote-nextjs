39d802f96213734b41ae07c657c6db458be5025b
devhasibulislam <devhasibulislam@gmail.com>
2024-11-19 15:53:38 +0600

    🎉 init(editor): rich text editor & preview

---
+++ src/app/layout.tsx
@@ -0,0 +1,51 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+import Script from "next/script";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "Summernote Next.js",
+  description: "Summernote Next.js example run in Next.js 15",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <head>
+        <link
+          href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.css"
+          rel="stylesheet"
+        />
+      </head>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {/* Load jQuery before Summernote */}
+        <Script
+          src="https://code.jquery.com/jquery-3.6.0.min.js"
+          strategy="beforeInteractive"
+        />
+        <Script
+          src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.js"
+          strategy="beforeInteractive"
+        />
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,91 @@
+"use client";
+
+import { useEffect, useRef, useState } from "react";
+
+function App() {
+  const [content, setContent] = useState<string>(
+    "<p>The fastest way to get <b>Summernote</b> is to download the precompiled and minified versions of our CSS and JavaScript. No documentation or original source code files are included.</p>"
+  );
+  const editorRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (typeof window !== "undefined" && editorRef.current) {
+      const $ = (window as any).$;
+      $(editorRef.current).summernote({
+        placeholder: content,
+        tabsize: 2,
+        height: 500,
+        toolbar: [
+          ["insert", ["picture", "link", "video", "table", "hr"]],
+          ["fontstyle", ["fontname", "fontsize", "fontsizeunit"]],
+          [
+            "font",
+            [
+              "bold",
+              "italic",
+              "underline",
+              "strikethrough",
+              "superscript",
+              "subscript",
+              "clear",
+            ],
+          ],
+          ["history", ["undo", "redo"]],
+          ["color", ["forecolor", "backcolor"]],
+          ["para", ["ol", "ul", "paragraph", "height"]],
+          ["misc", ["fullscreen", "codeview", "help"]],
+          [
+            "style",
+            [
+              "style",
+              "p",
+              "blockquote",
+              "pre",
+              "h1",
+              "h2",
+              "h3",
+              "h4",
+              "h5",
+              "h6",
+            ],
+          ],
+        ],
+        codeviewFilter: false,
+        codeviewIframeFilter: true,
+        callbacks: {
+          onChange: (contents: string) => {
+            setContent(contents);
+          },
+        },
+      });
+    }
+
+    return () => {
+      if (editorRef.current) {
+        const $ = (window as any).$;
+        $(editorRef.current).summernote("destroy");
+      }
+    };
+  }, []);
+
+  return (
+    <section className="h-screen w-screen">
+      <div className="h-full w-full p-4 grid grid-cols-12 gap-x-4">
+        <div className="md:col-span-6 col-span-12 flex flex-col gap-y-8">
+          <h1 className="lg:text-5xl md:text-3xl text-xl lg:font-bold md:font-semibold font-medium">
+            Summernote in React
+          </h1>
+          <div ref={editorRef} id="summernote"></div>
+        </div>
+        <div className="md:col-span-6 col-span-12 flex flex-col gap-y-8">
+          <h1 className="lg:text-5xl md:text-3xl text-xl lg:font-bold md:font-semibold font-medium">
+            Content Preview
+          </h1>
+          <div dangerouslySetInnerHTML={{ __html: content }}></div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default App;
